f171ad4b361c9932a75c87411770dd256e748848
Julian van Dijk <j.vandijk@coolblue.nl>
2025-12-05 08:03:43 +0100

    day 5

--- day5/part1.py
+++ day5/part1.py
@@ -1,6 +1,29 @@
 if __name__ == '__main__':
-
-    inputs = []
+    lines = []
     with open("./input", "r") as file:
-        inputs = [line.strip() for line in file.readlines()]
+        lines = [line.strip() for line in file.readlines()]
 
+separator_index = lines.index('')
+
+ranges = []
+for line in lines[:separator_index]:
+    start, end = line.split('-')
+    ranges.append([int(start), int(end)])
+
+ids = [int(line) for line in lines[separator_index + 1:]]
+ranges = sorted(ranges, key=lambda x: x[0])
+stack = [ranges[0]]
+for r in ranges[1:]:
+    if r[0] > stack[-1][1]:
+        stack.append(r)
+    else:
+        p = stack.pop()
+        stack.append((min(p[0], r[0]), max(p[1], r[1])))
+
+valid_ids = 0
+for id in ids:
+    for i in stack:
+        if id in range(i[0], i[1]+1):
+            valid_ids += 1
+            break
+print("valid IDs:", valid_ids)
--- day5/part2.py
+++ day5/part2.py
@@ -1,7 +1,22 @@
-
 if __name__ == '__main__':
-    inputs = []
+    lines = []
     with open("./input", "r") as file:
-        inputs = [line.strip() for line in file.readlines()]
+        lines = [line.strip() for line in file.readlines()]
 
+separator_index = lines.index('')
 
+ranges = []
+for line in lines[:separator_index]:
+    start, end = line.split('-')
+    ranges.append([int(start), int(end)])
+
+ids = [int(line) for line in lines[separator_index + 1:]]
+ranges = sorted(ranges, key=lambda x: x[0])
+stack = [ranges[0]]
+for r in ranges[1:]:
+    if r[0] > stack[-1][1]:
+        stack.append(r)
+    else:
+        p = stack.pop()
+        stack.append((min(p[0], r[0]), max(p[1], r[1])))
+print("Total fresh IDs:", sum([r[1]-r[0]+1 for r in stack]))
