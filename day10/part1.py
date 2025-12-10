9ad365f3e837df47cc47035aaf09f654be01ace9
Julian van Dijk <j.vandijk@coolblue.nl>
2025-12-10 08:11:38 +0100

    day 10

--- day10/part1.py
+++ day10/part1.py
@@ -1,6 +1,70 @@
-if __name__ == '__main__':
+from collections import deque
 
+
+def parse_line(line):
+    target_start = line.index('[') + 1
+    target_end = line.index(']')
+    target = line[target_start:target_end]
+    target_state = tuple(1 if c == '#' else 0 for c in target)
+
+    buttons = []
+    i = target_end + 1
+    while i < len(line):
+        if line[i] == '(':
+            j = line.index(')', i)
+            button_str = line[i + 1:j]
+            button = tuple(map(int, button_str.split(',')))
+            buttons.append(button)
+            i = j + 1
+        else:
+            i += 1
+
+    return target_state, buttons
+
+
+def state_after_press(state, button):
+    new_state = list(state)
+    for light in button:
+        new_state[light] = 1 - new_state[light]
+    return tuple(new_state)
+
+
+def min_presses(target_state, buttons):
+    n_lights = len(target_state)
+    initial_state = tuple([0] * n_lights)
+
+    if initial_state == target_state:
+        return 0
+
+    queue = deque([(initial_state, 0)])
+    visited = {initial_state}
+
+    while queue:
+        state, presses = queue.popleft()
+
+        for button in buttons:
+            new_state = state_after_press(state, button)
+
+            if new_state == target_state:
+                return presses + 1
+
+            if new_state not in visited:
+                visited.add(new_state)
+                queue.append((new_state, presses + 1))
+
+    return -1
+
+
+if __name__ == '__main__':
     inputs = []
     with open("./input", "r") as file:
         inputs = [line.strip() for line in file.readlines()]
 
+    total_presses = 0
+    for line in inputs:
+        if line:
+            target_state, buttons = parse_line(line)
+            min_p = min_presses(target_state, buttons)
+            total_presses += min_p
+
+    print(total_presses)
--- day10/part2.py
+++ day10/part2.py
@@ -1,7 +1,65 @@
+from scipy.optimize import linprog, milp, Bounds, LinearConstraint
+import numpy as np
+
+
+def parse_line_part2(line):
+    buttons = []
+    i = 0
+    while i < len(line):
+        if line[i] == '(':
+            j = line.index(')', i)
+            button_str = line[i + 1:j]
+            button = list(map(int, button_str.split(',')))
+            buttons.append(button)
+            i = j + 1
+        elif line[i] == '{':
+            break
+        else:
+            i += 1
+
+    jolt_start = line.index('{') + 1
+    jolt_end = line.index('}')
+    joltages = list(map(int, line[jolt_start:jolt_end].split(',')))
+
+    return buttons, joltages
+
+
+def min_button_presses(buttons, joltages):
+    n_counters = len(joltages)
+    n_buttons = len(buttons)
+
+    A = np.zeros((n_counters, n_buttons), dtype=float)
+    for j, button in enumerate(buttons):
+        for counter in button:
+            A[counter][j] = 1
+
+    c = np.ones(n_buttons)
+
+    constraints = LinearConstraint(A, lb=joltages, ub=joltages)
+
+    bounds = Bounds(lb=0, ub=np.inf)
+
+    integrality = np.ones(n_buttons)
+
+    result = milp(c=c, constraints=constraints, bounds=bounds, integrality=integrality)
+
+    if result.success:
+        return int(np.sum(result.x))
+
+    return -1
+
 
 if __name__ == '__main__':
     inputs = []
     with open("./input", "r") as file:
         inputs = [line.strip() for line in file.readlines()]
 
+    total_presses = 0
+    for line in inputs:
+        if line:
+            print(line)
+            buttons, joltages = parse_line_part2(line)
+            min_p = min_button_presses(buttons, joltages)
+            total_presses += min_p
 
+    print(total_presses)
